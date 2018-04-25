ea955d54388e880a89720fe2300f19b33d4fc1bd
danfanjun <121294592@qq.com>
2018-04-25 05:33:08 +0000

    helpers

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,7 +3,7 @@
 function get_db_config(){
     if(getevn('IS_IN_HEROKU')){
          $url = parse_url(getenv("DATABASE_URL"));
-         return $db_config[
+        return $db_config= [
             'connection' => 'pgsql',
             'host'       => $url["host"],
             'database'   => substr($url["path"], 1),
@@ -11,7 +11,7 @@ function get_db_config(){
             'password'   => $url["pass"],
          ];
     }else{
-        return $db_config[
+        return $db_config= [
             'connection' => evn('DB_CONNECTION' , 'mysql'),
             'host'       => evn('DB_HOST' , 'localhost'),
             'database'   => evn('DB_DATABASE' , 'forge'),
